598b29998c23f13396a5b3d8b154ac7eda393b29
Marcelo Martins <m.a.martins@live.com>
2019-05-27 11:51:40 -0300

    Added CSS classes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+export class User {
+  name: string;
+  email: string;
+  birthDate: Date;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ant-demo';
+export class AppComponent implements OnInit {
+  dateFormat = 'dd/MM/yyyy';
+  form: FormGroup;
+  user = new User();
+  collapsed = false;
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.user.name = 'Marcelo Martins';
+    this.user.email = 'm.a.martins@live.com';
+    this.user.birthDate = new Date(1974, 6, 12);
+    this.form = this.fb.group({
+      name: [null, Validators.required],
+      email: [null, [Validators.email, Validators.required]],
+      birthDate: [null, Validators.required]
+    });
+
+    this.form.patchValue(this.user);
+  }
+
+  onSubmit() {
+    for (const key in this.form.controls) {
+      this.form.controls[key].markAsDirty();
+      this.form.controls[key].updateValueAndValidity();
+    }
+    this.user = this.form.value;
+    console.log(JSON.stringify(this.user));
+    console.log(this.diagnostic);
+    console.log(this.form.valid);
+    console.log(this.form.get('name').hasError('required'));
+    console.log(this.user.birthDate.toISOString());
+  }
+
+  toggle(): boolean {
+    return this.collapsed = !this.collapsed;
+  }
+
+  get name(): AbstractControl {
+    return this.form.get('name');
+  }
+
+  get email(): AbstractControl {
+    return this.form.get('email');
+  }
+
+  get birthDate(): AbstractControl {
+    return this.form.get('birthDate');
+  }
+
+  get diagnostic() {
+    return JSON.stringify(this.form.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgZorroAntdModule, NZ_I18N, pt_BR } from 'ng-zorro-antd';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import pt from '@angular/common/locales/pt';
+
+registerLocaleData(pt);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+
+    NgZorroAntdModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: pt_BR }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
